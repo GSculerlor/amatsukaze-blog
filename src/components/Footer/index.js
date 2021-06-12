ab791ad57ef15ef17212c124e6641428e4b9fb7a
Ganendra Afrasya <gsculerlor@hotmail.com>
2021-06-13 02:04:16 +0700

    update footer wording

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,7 +5,7 @@ import P from './P'
 const Footer = () => {
     return (
       <Wrapper>
-        <P>made when I was bored <span role="img" aria-label="pancakes">🥞</span></P>
+        <P>made with <span role="img" aria-label="love">💖</span> when I was bored <span role="img" aria-label="pancakes">🥞</span></P>
       </Wrapper>
     );
   };
