226c49ac3c0533965ea78bbd38bd65692e0230dd
Ganendra Afrasya <gsculerlor@hotmail.com>
2021-06-13 00:12:28 +0700

    set .gatsby-hightlight to use default styles for now

--- src/global-styles.js
+++ src/global-styles.js
@@ -4,8 +4,6 @@ import styledNormalize from "styled-normalize";
 const GlobalStyle = createGlobalStyle`
   ${styledNormalize}
   
-  @import url("https://fonts.googleapis.com/css?family=Roboto:400,700");
-  
   *,
   *:before,
   *:after {
@@ -30,32 +28,6 @@ const GlobalStyle = createGlobalStyle`
 
   img {
     max-width: 100%;
-  }
-
-  .gatsby-highlight {
-    border-bottom: 1px solid #e0e6ed;
-    border-top: 1px solid #e0e6ed;
-    margin: 0px -75px;
-    padding: 0;
-
-    pre[class*="language-"] {
-      margin: 0;
-      padding: 25px 50px;
-    }
-  }
-
-  pre[class*="language-"] {
-    background: rgba(245, 245, 245, 1);
-    color: rgb(65, 76, 94);  }
-
-  @media only screen and (max-width: 870px) {
-    .gatsby-highlight {
-      margin: 15px -15px;
-
-      pre[class*="language-"] {
-        padding: 25px;
-      }
-    }
   }
 `;
 export default GlobalStyle;
